a072d06def8f1b3cfddcbd80adb9b16b902e558f
matthoare117 <dev+github@matthoare117.dev>
2026-07-09 00:23:12 -0500

    sitelog 0.0.11

--- Casks/sitelog.rb
+++ Casks/sitelog.rb
@@ -1,8 +1,8 @@
 cask "sitelog" do
-  version "0.0.10"
-  sha256 "86c289db26c2c26cc77efdeb734deacd099a1b02da11d9c90083cc6cf452f7eb"
+  version "0.0.11"
+  sha256 "62e069a4c31c2664f31d992216cf16f053a5dd0ca47ef28cabfb8a72fd429191"
 
-  url "https://github.com/matthoare117/homebrew-tools/releases/download/sitelog-v0.0.10/Sitelog-0.0.10-arm64-mac.zip"
+  url "https://github.com/matthoare117/homebrew-tools/releases/download/sitelog-v0.0.11/Sitelog-0.0.11-arm64-mac.zip"
   name "Sitelog"
   desc "Sitelog desktop app"
   homepage "https://github.com/matthoare117/workshop"
