45704e30156e9c42befef2aada74886aa05581c4
Matthew Hoare <matty@mac-mini.local>
2026-07-10 12:15:29 -0500

    sitelog 0.0.21 [skip ci]

--- Casks/sitelog.rb
+++ Casks/sitelog.rb
@@ -1,8 +1,8 @@
 cask "sitelog" do
-  version "0.0.20"
-  sha256 "ce8b478be4cc42c86f10f4d43a35529cd31530dc5e6c2e06adea3217b744f769"
+  version "0.0.21"
+  sha256 "6374a38f8746a8429a85ceeba8cbb07cd6d376785d3c3242d8a0182b05e0f3de"
 
-  url "https://github.com/matthoare117/homebrew-tools/releases/download/sitelog-v0.0.20/Sitelog-0.0.20-arm64-mac.zip"
+  url "https://github.com/matthoare117/homebrew-tools/releases/download/sitelog-v0.0.21/Sitelog-0.0.21-arm64-mac.zip"
   name "Sitelog"
   desc "Sitelog desktop app"
   homepage "https://github.com/matthoare117/workshop"
