6b4034b7e8b8bf5dd5ed1c18be81423b30ab363e
matthoare117 <dev+github@matthoare117.dev>
2026-07-23 11:32:50 -0500

    toolchest 0.0.30 [skip ci]

--- Casks/toolchest.rb
+++ Casks/toolchest.rb
@@ -1,8 +1,8 @@
 cask "toolchest" do
-  version "0.0.29"
-  sha256 "1cd4325a3af5747d70914b827c0818c7af015d987bff97202897f72493bf56f2"
+  version "0.0.30"
+  sha256 "c6fdc3f40f31c760a47447e30ef87cd6151e0f895af1faf53fd476ef78834454"
 
-  url "https://github.com/matthoare117/homebrew-tools/releases/download/toolchest-v0.0.29/toolchest-0.0.29-arm64-mac.zip"
+  url "https://github.com/matthoare117/homebrew-tools/releases/download/toolchest-v0.0.30/toolchest-0.0.30-arm64-mac.zip"
   name "Toolchest"
   desc "Toolchest desktop app"
   homepage "https://github.com/matthoare117/workshop"
